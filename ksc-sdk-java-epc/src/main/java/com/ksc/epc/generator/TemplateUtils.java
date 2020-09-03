5edcff02c4606c914d3c300b30301694a8385f3c
wangxiaohan1 <wangxiaohan1@kingsoft.com>
2020-09-03 19:15:03 +0800

    codegen

--- ksc-sdk-java-epc/src/main/java/com/ksc/epc/generator/TemplateUtils.java
+++ ksc-sdk-java-epc/src/main/java/com/ksc/epc/generator/TemplateUtils.java
@@ -9,9 +9,22 @@ import java.lang.reflect.Field;
  */
 public class TemplateUtils {
 
-    public static void generateForRequest(Class clazz){
+    public static void generateForRequest(Class clazz) {
         for (Field field : clazz.getDeclaredFields()) {
-            if(field)
+            Class<?> fieldClazz = field.getType();
+            if (fieldClazz.equals(String.class)) {
+
+            } else if (fieldClazz.equals(Integer.class) || fieldClazz.equals(int.class)) {
+
+            } else if (fieldClazz.equals(Long.class) || fieldClazz.equals(long.class)) {
+
+            } else if (fieldClazz.equals(Double.class) || fieldClazz.equals(double.class)) {
+
+            } else if (fieldClazz.equals(Float.class) || fieldClazz.equals(float.class)) {
+
+            } else if (fieldClazz.equals(Boolean.class) || fieldClazz.equals(boolean.class)) {
+
+            }
         }
     }
 }
